5dd8495de43d0014e5b5b771258b8b2f420d37e0
funnybr0ther <guillaumevdrest@gmail.com>
2020-04-24 10:47:39 +0200

    basic implementation of product layout

---
+++ Wishlist/app/src/main/java/com/example/wishlist/Activities/ViewProductActivity.java
@@ -0,0 +1,18 @@
+package com.example.wishlist.Activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ViewProductActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        if()
+
+    }
+}
