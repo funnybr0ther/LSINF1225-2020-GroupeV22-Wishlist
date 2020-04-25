5d9aad043f093951f4da46e3a3c66e8d41c51919
Arthur Vandroogenbroek <arthur.vandroogenbroek@student.uclouvain.be>
2020-04-25 21:04:30 +0100

    Implémentation de modification de profil

--- Wishlist/app/src/main/java/com/example/wishlist/Activities/MainMenuActivity.java
+++ Wishlist/app/src/main/java/com/example/wishlist/Activities/MainMenuActivity.java
@@ -7,27 +7,33 @@ import androidx.core.app.ActivityCompat;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.wishlist.R;
 
 public class MainMenuActivity extends AppCompatActivity {
-
+    private int userID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Intent intent=getIntent();
         if (intent.hasExtra("userID")){
-            Integer userID= intent.getIntExtra("userID",-1);
+            userID= intent.getIntExtra("userID",-1);
             setContentView(R.layout.activity_main_menu);
             TextView textView=findViewById(R.id.textView);
-            textView.setText(userID.toString());
+            textView.setText(Integer.toString(userID));
         }
         else{//If no userID go back to LoginActivity
             //Toast toast=new Toast(this,);
             Intent backToLogin=new Intent(this,LoginActivity.class);
         }
+    }
+    public void MyProfile(View view){
+        Intent intent=new Intent(this,MyProfileActivity.class);
+        intent.putExtra("userID",userID);
+        startActivity(intent);
     }
 }
