07b979d91c8ad0a1ae3b8dd1f0748825a43814ff
usoltsev <alexandr.usoltsev@student.uclouvain.be>
2020-05-01 15:24:02 +0200

    basic implementation of follow list with no access nor sql database sync

--- Wishlist/app/src/main/java/com/example/wishlist/Class/FollowRecyclerAdapter.java
+++ Wishlist/app/src/main/java/com/example/wishlist/Class/FollowRecyclerAdapter.java
@@ -1,6 +1,8 @@
 package com.example.wishlist.Class;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,21 +15,41 @@ import java.util.ArrayList;
 
 public class FollowRecyclerAdapter extends RecyclerView.Adapter<FollowRecyclerAdapter.Viewholder> {
 
-    private ArrayList<User> follows = new ArrayList<>();
+    private ArrayList<User> followList;
 
     public FollowRecyclerAdapter(ArrayList<User> follows) {
-        this.follows = follows;
+        this.followList = follows;
     }
 
-    public class Viewholder extends RecyclerView.ViewHolder{
+    @NonNull
+    @Override
+    public Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.follow_list_item,parent,false);
+        return new Viewholder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Viewholder holder, int position) {
+
+        String full_name = followList.get(position).getFirstName()+" "+followList.get(position).getLastName();
+        holder.name.setText(full_name);
+        //holder.picture.set comment on set des images mdrrr?
+
+    }
+
+    @Override
+    public int getItemCount() { return followList.size(); }
+
+
+     class Viewholder extends RecyclerView.ViewHolder{
 
         TextView name;
         ImageView picture;
 
-        public Viewholder(@NonNull View itemView) {
+         Viewholder(@NonNull View itemView) {
             super(itemView);
-            name = itemView.findViewById(R.id.name);
-            picture = itemView.findViewById(R.id.picture);
+            name = itemView.findViewById(R.id.followName);
+            picture = itemView.findViewById(R.id.followPicture);
         }
     }
 }
