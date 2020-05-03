31e389e9b1f5d0932fc18ab127cc63c094ff1e7f
lvalkenberg <louis.valkenberg@student.uclouvain.be>
2020-05-03 19:08:23 +0200

    finalisation du button add wishlist

--- Wishlist/app/src/main/java/com/example/wishlist/Fragment/AddWishlistFragment.java
+++ Wishlist/app/src/main/java/com/example/wishlist/Fragment/AddWishlistFragment.java
@@ -1,11 +1,13 @@
 package com.example.wishlist.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -28,16 +30,38 @@ public class AddWishlistFragment extends DialogFragment {
         txtName = (EditText) rootView.findViewById(R.id.nameCreateWishlist);
         btnCreate = (Button) rootView.findViewById(R.id.buttonCreateWishlist);
 
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String listName = txtName.getText().toString();
+                if(listName.matches("")) {
+                    Toast toast = Toast.makeText(getContext(), "Wrong name", Toast.LENGTH_SHORT);
+                    toast.show();
+                }else if(!listName.matches("")) {
+                    WishlistDatabaseHelper db = new WishlistDatabaseHelper(getActivity().getApplicationContext());
+                    int userID = getArguments().getInt("userID");
+                    db.addWishlist(listName, userID);
+                    Toast toast=Toast.makeText(getContext(),"Wishlist " + listName + " created !",Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                AddWishlistFragment.this.getDialog().dismiss();
+            }
+        });
         return rootView;
     }
 
 
 
-    public void PressBtnCreate(View view){
-        if(txtName.getText().toString() != "") {
-            WishlistDatabaseHelper db = new WishlistDatabaseHelper(getContext());
+    /*public void PressBtnCreate(View view){
+        String listName = txtName.getText().toString();
+        if(listName != "") {
+            WishlistDatabaseHelper db = new WishlistDatabaseHelper(getActivity().getApplicationContext());
 
+            Toast toast=Toast.makeText(getContext(),"Wishlist " + listName + " created !",Toast.LENGTH_SHORT);
+            toast.show();
         }
-    }
+        Toast toast=Toast.makeText(getContext(),"Wrong name",Toast.LENGTH_SHORT);
+        toast.show();
+    }*/
 
 }
