84e17604c166cb7bfc1b7e9d43096421a8dfb10b
lvalkenberg <louis.valkenberg@student.uclouvain.be>
2020-05-05 22:13:57 +0200

    product list fini

--- Wishlist/app/src/main/java/com/example/wishlist/Activities/DetailWishlistActivity.java
+++ Wishlist/app/src/main/java/com/example/wishlist/Activities/DetailWishlistActivity.java
@@ -12,8 +12,10 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.example.wishlist.Adapters.ProductAdapter;
 import com.example.wishlist.Adapters.WishlistAdapter;
 import com.example.wishlist.Class.Product;
+import com.example.wishlist.Class.ProductDatabaseHelper;
 import com.example.wishlist.Class.Wishlist;
 import com.example.wishlist.Class.WishlistDatabaseHelper;
 import com.example.wishlist.Fragment.AddWishlistFragment;
@@ -26,6 +28,7 @@ public class DetailWishlistActivity extends AppCompatActivity {
     private int userID;
     private int wishlistID;
     WishlistDatabaseHelper dbWishlist;
+    ArrayList<Product> products;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,12 +54,18 @@ public class DetailWishlistActivity extends AppCompatActivity {
         }
 
         //Va chercher dans la BDD les product d'une wishlist grace a sa wishlistID
-        int[] productID = dbWishlist.getProducts(wishlistID);
+        ListView wishlistListView = findViewById(R.id.wishlist_DetailView);
+        products = getProductArray();
+        wishlistListView.setAdapter(new ProductAdapter(this, products));
     }
 
-    public ArrayList<Product> getProductFromReference(int[] productID){
+    public ArrayList<Product> getProductArray(){
+        ProductDatabaseHelper dbProduct = new ProductDatabaseHelper(getApplicationContext());
         ArrayList<Product> products = new ArrayList<Product>();
-        //TODO
+        int[] prod = dbWishlist.getProducts(wishlistID);
+        for(int productID : prod){
+            products.add(dbProduct.getProductFromID(productID));
+        }
         return products;
     }
 
@@ -67,10 +76,18 @@ public class DetailWishlistActivity extends AppCompatActivity {
 
     }
 
+    public void productDetail(int productPosition, boolean isMyProduct){
+        Intent intent1=new Intent(this, ViewProductActivity.class);
+        intent1.putExtra("productID",dbWishlist.getProducts(wishlistID)[productPosition]);
+        intent1.putExtra("userID",userID);
+        intent1.putExtra("isMyProduct",isMyProduct);
+        startActivity(intent1);
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 1){
+        if(requestCode == 4){
             if (resultCode == RESULT_OK) {
                 if(data.hasExtra("newProduct")) {
                     int productID = data.getIntExtra("newProduct", -1);
