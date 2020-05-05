5d0c9937ac1479b14fb26912c214be6c02794790
funnybr0ther <guillaumevdrest@gmail.com>
2020-05-06 00:14:54 +0200

    Fully safe field-filling with wrong information warning

--- Wishlist/app/src/main/java/com/example/wishlist/Activities/DetailWishlistActivity.java
+++ Wishlist/app/src/main/java/com/example/wishlist/Activities/DetailWishlistActivity.java
@@ -70,7 +70,7 @@ public class DetailWishlistActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == 1){
+        if(requestCode == 4){
             if (resultCode == RESULT_OK) {
                 if(data.hasExtra("newProduct")) {
                     int productID = data.getIntExtra("newProduct", -1);
