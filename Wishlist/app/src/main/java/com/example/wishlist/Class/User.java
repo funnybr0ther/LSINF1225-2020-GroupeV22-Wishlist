a97afbcc89c1fe409a41b72d3f41deeec23a2e06
funnybr0ther <guillaumevdrest@gmail.com>
2020-05-08 16:33:48 +0200

    create apk for app

--- Wishlist/app/src/main/java/com/example/wishlist/Class/User.java
+++ Wishlist/app/src/main/java/com/example/wishlist/Class/User.java
@@ -10,7 +10,7 @@ import java.util.Date;
 import java.util.Objects;
 
 public class User {
-    private Address address; 
+    private Address address;
     private String firstName;
     private String lastName;
     private String email;
